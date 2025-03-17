491c4e38ddc0b8305cce754fb9a6760873ae0aa5
junx8 <junx.chen@intel.com>
2025-03-17 19:14:06 +0800

    Update training_yolo.py

--- training_yolo.py
+++ training_yolo.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
+if __name__ == '__main__':
+    # Load a model
+    model = YOLO("yolov8s.pt")  # build from YAML and transfer weights
 
-# Train the model
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640, device='xpu')
+    # Train the model
+    results = model.train(data="coco128.yaml", epochs=100, imgsz=2048, batch=8, device='xpu')
